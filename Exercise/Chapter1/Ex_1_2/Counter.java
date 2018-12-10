3baa4b5b2164e82aaf25ff48d518a3c627b73d30
shzhou <RagdollNoone@163.com>
2018-12-10 16:10:43 +0800

    stage commit

---
+++ Exercise/Chapter1/Ex_1_2/Counter.java
@@ -0,0 +1,34 @@
+package Exercise.Chapter1.Ex_1_2;
+
+public class Counter
+implements Comparable<Counter>{
+    private final String name;
+    private int count;
+
+    public Counter(String name)
+    {
+        this.name = name;
+    }
+
+    public void increment()
+    {
+        count++;
+    }
+
+    public int getTally()
+    {
+        return count;
+    }
+
+    public String toString()
+    {
+        return count + " " + this.name;
+    }
+
+    public int compareTo(Counter that)
+    {
+        if (this.count < that.count) return -1;
+        if (this.count > that.count) return 1;
+        else return 0;
+    }
+}
---
+++ Exercise/Chapter1/Ex_1_2/Ex_1_2_6.java
@@ -0,0 +1,20 @@
+package Exercise.Chapter1.Ex_1_2;
+
+public class Ex_1_2_6 {
+    public static void DoExercise()
+    {
+        String s1 = "ACTGACG";
+        String s2 = "TGACGAC";
+
+        boolean result = checkCircularRotation(s1, s2);
+        System.out.println("Result is: " + result);
+    }
+
+    private static boolean checkCircularRotation(String s1, String s2)
+    {
+        if((s1 + s1).indexOf(s2) != -1 && s1.length() == s2.length())
+            return true;
+
+        return false;
+    }
+}
